7bd0bf99921853681c6b18fe1713f8ffb3c447bf
Kelvin Helmut <aek6.io@gmail.com>
2026-02-18 11:54:26 -0500

    chore: clean files

--- src/routes/about.svelte
+++
@@ -1,8 +0,0 @@
-<svelte:head>
-	<title>About | Devitai</title>
-</svelte:head>
-
-<h1>About us</h1>
-
-<p><strong>Dev</strong>elopment of applications, <strong>I</strong>nformation <strong>T</strong>echnologies and <strong>A</strong>rtificial <strong>I</strong>ntelligence.</p>
-<p>WhatsApp: <a href="https://api.whatsapp.com/send?phone=+51954733536" target="_blank">+51 954733536</a></p>
